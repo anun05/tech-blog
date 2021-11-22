ca9a252a90c2e69a1cae77c12498f900bd92c9fa
Alex Nunez <alexnunez957@gmail.com>
2021-11-22 14:58:37 -0800

    adding some files to hw

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const { Project, User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
